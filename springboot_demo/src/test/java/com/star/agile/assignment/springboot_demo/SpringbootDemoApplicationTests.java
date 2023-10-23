96c8d6adc264109319a7e741aa050c15979af14c
vijayalakshmipushpendra <vijayalakshmi1812@gmail.com>
2023-10-23 15:14:45 +0530

    First commit

---
+++ springboot_demo/src/test/java/com/star/agile/assignment/springboot_demo/SpringbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.star.agile.assignment.springboot_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
